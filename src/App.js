9428576b6dd34b76d76d3e3a8d05433b325d91ba
Tarun Gorai <goraitarunkumar@gmail.com>
2020-12-08 08:01:17 +0530

    fixed the states in app.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import GoogleLoginComponent from "./components/GoogleLoginComponent";
 import Layout from "./components/Layout/Layout"
 class App extends React.Component {
   state = {
-    showBackdrop: false,
-    showMobileNav: false,
     isAuth: false,
     token: null,
     userId: null,
     authLoading: false,
     error: null,
-    apiBaseUrl: "http://localhost:8001",
+    apiBaseUrl: "http://localhost:8000",
   };
   componentDidMount() {
     const token = localStorage.getItem("token");
