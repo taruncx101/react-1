03a7387176bf5d94a3eed166d7ce0478a25c0d8b
Tarun Gorai <goraitarunkumar@gmail.com>
2020-12-08 14:08:38 +0530

    fetching users

---
+++ src/components/User/UserCard.js
@@ -0,0 +1,14 @@
+const UserCard = (props) => {
+    return (
+      <div className="col-sm-3">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">{props.user.name}</h5>
+            <p className="card-text">{props.user.email}</p>
+            <button className="btn btn-primary">Details</button>
+          </div>
+        </div>
+      </div>
+    );
+}
+export default UserCard;
--- src/pages/User/Users.js
+++ src/pages/User/Users.js
@@ -1,7 +1,51 @@
-export default function User(params) {
-    return (
-        <div>
-            This is from users component
+import React from 'react';
+import UserCard from '../../components/User/UserCard'
+export default class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        users: [],
+        totalUsers: 0,
+        currentPage: 1,
+    };
+  }
+componentDidMount() {
+    this.loadUsers(1);    
+}
+  loadUsers = (page) => {
+    const url = `${this.props.apiBaseUrl}/user/user-list?page=${page}`;
+    fetch(url, {
+      headers: {
+        Authorization: "Bearer " + this.props.token,
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new Error("Failed to fetch users.");
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        this.setState({
+          users: resData.rows,
+          totalUsers: resData.count,
+        });
+      })
+    .catch(err => {
+        console.log(err)
+    });
+  };
+    render() {
+      return (
+          <div className="row">
+          <div className="col-sm-12" style={{ paddingTop: '50px'}}>
+            <h5>Total: {this.state.totalUsers}</h5>
+          </div>
+          {this.state.users.map((user) => (
+            <UserCard user={user} />
+          ))}
+          <div className="col-sm-12"></div>
         </div>
-    )
+      );
+  }
 }
